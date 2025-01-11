e147d7264ba82b2e967a3703289b978271447c37
Samuraicode3s <roninkva@gmail.com>
2025-01-10 21:10:47 -0500

    added checkout api

--- Backend/index.js
+++ Backend/index.js
@@ -1,21 +1,46 @@
-const express = require('express')
+const express = require("express");
+const AWS = require("aws-sdk");
+const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
+const app = express();
+app.use(bodyParser.json());
+require("dotenv").config();
+AWS.config.update({
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const TABLE_NAME = "orders";
+app.post("/checkout", async (req, res) => {
+  const { customerId, productName, quantity } = req.body;
+  if (!customerId || !productName || !quantity) {
+    return res.status(400).json({
+      message: "Please provide customerId, productName, and quantity.",
+    });
+  }
+  const params = {
+    TableName: TABLE_NAME,
+    Item: {
+      id: uuidv4(),
+      customerId: customerId,
+      productname: productName,
+      quantity: quantity,
+    },
+  };
+  try {
+    await dynamoDB.put(params).promise();
+    res
+      .status(201)
+      .json({ message: "Order inserted successfully", order: params.Item });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Could not insert order", error: error.message });
+  }
+});
 
-const app = express()
-
-const port = 3000
-
- 
-
-app.get('/', (req, res) => {
-
-  res.json('Hello World!')
-
-})
-
- 
-
-app.listen(port, () => {
-
-  console.log(`Example app listening on port ${port}`)
-
-})
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
