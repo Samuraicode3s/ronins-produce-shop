c365f6e056d3be9bf06cdfab25a0207191727140
Samuraicode3s <roninkva@gmail.com>
2025-01-11 22:49:20 -0500

    Created beanstalk app and confirmed on Postman

--- Backend/index.js
+++ Backend/index.js
@@ -40,7 +40,7 @@ app.post("/checkout", async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
